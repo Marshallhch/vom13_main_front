4d5574ed05644f56b9ca949e246293ef6e00a134
VomEdu <apple@appleui-iMac.local>
2023-04-26 12:48:24 +0900

    md art 탭 기능 준비

--- js/ui/customjs.js
+++ js/ui/customjs.js
@@ -1,5 +1,22 @@
+/*-------- HEADER HIDE AND SHOW --------*/
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > 150) {
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = 0;
+    } else {
+      header.style.top = -100 + "%";
+    }
+    prevScrollpos = currentScrollPos; // 마우스 이동 후 스크롤 위치값 재할당
+  }
+});
+
 /*-------- BEST ITEMS SLIDE --------*/
-const swiper = new Swiper(".best-image-wrapper .swiper", {
+const bestArtSwiper = new Swiper(".best-image-wrapper .swiper", {
   slidesPerView: 4,
   spaceBetween: 15,
 
