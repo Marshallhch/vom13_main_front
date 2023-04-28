220e4ea8bbdb4e7cba5175e90df4596261c7b602
marshallhch <marshallhch@gmail.com>
2023-04-28 12:50:15 +0900

    new art section 작업 중 0428

---
+++ js/ui/customjq.js
@@ -0,0 +1,106 @@
+$(function () {
+  /* -- 헤더 높이만큼 페딩 적용 */
+  const headerHeight = $("header").outerHeight();
+  $(".landing").css("padding-top", `${headerHeight}px`);
+  $(window).on("resize", function () {
+    const headerHeight = $("header").outerHeight();
+    $(".landing").css("padding-top", `${headerHeight}px`);
+  });
+
+  // 메인 랜딩 슬라이더
+  /*===== VISUAL SLIDER FUNCTION ===== */
+  function visualSlider() {
+    var visualSlider = new Swiper(".landing-slider", {
+      loop: true,
+      speed: 1000,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      centeredSlides: true,
+      observer: true,
+      observeParents: true,
+      watchSlidesProgress: true,
+      pagination: {
+        el: ".landing .swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          var bulletArray = [];
+          $(".landing .swiper-slide").each(function () {
+            bulletArray.push($(this).find("span").html());
+          });
+          return `
+            <div class="${className} main-slide-bullet">
+              <span>0${index + 1}</span>
+              <em>${bulletArray[index + 1]}</em>
+            </div>
+          `;
+        },
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      on: {
+        init: function () {
+          $(".landing-slider").addClass("load-init");
+        },
+        progress: function () {
+          for (var i = 0; i < this.slides.length; i++) {
+            var slideProgress = this.slides[i].progress;
+            var innerOffset = this.width * 0.5;
+            var innerTranslate = slideProgress * innerOffset;
+            this.slides[i].querySelector(".bg-wrap").style.transform =
+              "translate3d(" + innerTranslate + "px, 0, 0)";
+          }
+        },
+        // touchStart: function () {
+        //   for (var i = 0; i < this.slides.length; i++) {
+        //     this.slides[i].style.transition = "";
+        //   }
+        // },
+        setTransition: function (speed) {
+          for (var i = 0; i < this.slides.length; i++) {
+            this.slides[i].style.transition = speed + "ms";
+            this.slides[i].querySelector(".bg-wrap").style.transition =
+              speed + "ms";
+          }
+        },
+      },
+    });
+
+    visualSlider.on("slideChange", function () {
+      $(".landing-slider").removeClass("load-init");
+    });
+  }
+  visualSlider();
+
+  /*-------- FITTING MD PICK IMAGE RATIO --------*/
+  function fitImageRatio(el1, el2, elv1, elv2) {
+    const mdImageWidth = $(el1).width();
+    const panelWidth = $(el2).width();
+    $(el1).height(mdImageWidth * elv1);
+    $(el2).height(panelWidth * elv2);
+  }
+
+  $(window).on("resize", function () {
+    fitImageRatio(
+      ".pick-tab-panel.on .panel-img",
+      ".pick-tab-panel.on .panel-desc",
+      0.8,
+      0.4
+    );
+  });
+  $(".pick-tab-btn").on("click", function () {
+    fitImageRatio(
+      ".pick-tab-panel.on .panel-img",
+      ".pick-tab-panel.on .panel-desc",
+      0.8,
+      0.4
+    );
+  });
+  fitImageRatio(
+    ".pick-tab-panel.on .panel-img",
+    ".pick-tab-panel.on .panel-desc",
+    0.8,
+    0.4
+  );
+});
---
+++ js/ui/customjs.js
@@ -0,0 +1,127 @@
+/*-------- HEADER HIDE AND SHOW --------*/
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > 150) {
+    header.classList.remove("top");
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = 0;
+    } else {
+      header.style.top = -100 + "%";
+    }
+    prevScrollpos = currentScrollPos; // 마우스 이동 후 스크롤 위치값 재할당
+  } else {
+    header.classList.add("top");
+  }
+});
+
+/*-------- MOBILE MENU HIDE AND SHOW --------*/
+const mobileMenuIcon = document.querySelector(".menu-icon");
+const mobileOverlay = document.querySelector(".mobile-overlay");
+const mobileCloseIcon = document.querySelector(".close-icon");
+mobileMenuIcon.addEventListener("click", function (e) {
+  e.preventDefault(); // default original effect prevented
+  mobileOverlay.classList.add("on");
+  document.body.style.overflow = "hidden"; // when menu activated, all scroll prevented
+});
+mobileCloseIcon.addEventListener("click", function (e) {
+  e.preventDefault();
+  mobileOverlay.classList.remove("on");
+  document.body.style.overflow = "auto";
+});
+
+// target, currentTarge
+mobileOverlay.addEventListener("click", function (e) {
+  // console.log(e.target); // point to clicked element
+  // console.log(e.currentTarget); // point to uppest element(최상위)
+  // console.log(e.target.nodeName);
+  if (
+    e.target.getAttribute("class") !== "mobile-menus" &&
+    e.target.nodeName !== "LI" &&
+    e.target.nodeName !== "A" &&
+    e.target.nodeName !== "IMG"
+  ) {
+    mobileOverlay.classList.remove("on");
+    document.body.style.overflow = "auto";
+  }
+});
+
+/*-------- BEST ITEMS SLIDE --------*/
+const bestArtSwiper = new Swiper(".best-image-wrapper .swiper", {
+  slidesPerView: 4,
+  spaceBetween: 15,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    480: {
+      // 480 이하
+      slidesPerView: 1, // 보이는 슬라이드 갯수
+      slidesPerGroup: 1, // 하나의 페이지네이션에 묶이는 슬라이드 갯수
+    },
+    880: {
+      // 880 이하
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1400: {
+      // 1400 이하
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+});
+
+/*-------- NEW ART SLIDE --------*/
+const newArtSwiper = new Swiper(".new-art-slider-wrapper .swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // breakpoints: {
+  //   480: {
+  //     // 480 이하
+  //     slidesPerView: 1, // 보이는 슬라이드 갯수
+  //     slidesPerGroup: 1, // 하나의 페이지네이션에 묶이는 슬라이드 갯수
+  //   },
+  //   880: {
+  //     // 880 이하
+  //     slidesPerView: 2,
+  //     slidesPerGroup: 2,
+  //   },
+  //   1400: {
+  //     // 1400 이하
+  //     slidesPerView: 3,
+  //     slidesPerGroup: 3,
+  //   },
+  // },
+});
+
+/*-------- MD PICK TBAS --------*/
+// 1. 요소 선택
+const btns = document.querySelectorAll(".pick-tab-btn");
+const panels = document.querySelectorAll(".pick-tab-panel");
+
+// 2. 함수 정의
+function activeTabs(i) {
+  btns.forEach((btn) => {
+    btn.classList.remove("on");
+  });
+  panels.forEach((panel) => {
+    panel.classList.remove("on");
+  });
+  btns[i].classList.add("on");
+  panels[i].classList.add("on");
+}
+
+// 3. 함수 호출
+btns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    activeTabs(idx);
+  });
+});
