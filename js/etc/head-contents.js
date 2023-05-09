788cf5813fcc27362e433df2cb654f64fbdd02bc
marshallhch <marshallhch@gmail.com>
2023-05-09 12:07:59 +0900

    호스팅 이전 0509

--- js/etc/head-contents.js
+++ js/etc/head-contents.js
@@ -9,66 +9,37 @@ if (pageURL.includes("index")) {
   pageName = "sign";
 }
 
-const fileNames = [
+/* ----- Custom CSS Files ----- */
+const cssFileNames = [
   "reset",
   "common",
   "animation",
   "header",
-  "main",
+  `${pageName}`,
   "footer",
   "responsive",
 ];
 
-for (let i = 0; i < fileNames.length; i++) {
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = `/baexang_front/css/${fileNames[i]}.css?_v=${versionUpdate}`;
-  document.head.insertAdjacentElement("beforeend", link);
+for (let i = 0; i < cssFileNames.length; i++) {
+  const cssFile = document.createElement("link");
+  cssFile.rel = "stylesheet";
+  cssFile.href = `/baexang_front/css/${cssFileNames[i]}.css?_v=${versionUpdate}`;
+  document.head.insertAdjacentElement("beforeend", cssFile);
 }
 
-/* ----- Custom CSS Files ----- */
-// const elmt = document.createElement("link");
-// const reset = document.createElement("link");
-// const common = document.createElement("link");
-// const animation = document.createElement("link");
-// const header = document.createElement("link");
-// const main = document.createElement("link");
-// const footer = document.createElement("link");
-// const responsive = document.createElement("link");
-
-// const fileNames = [reset, common, animation, header, main, footer, responsive];
-
-// reset.rel = "stylesheet";
-// reset.href = `/baexang_front/css/reset.css?_v=${versionUpdate}`;
-// common.rel = "stylesheet";
-// common.href = `/baexang_front/css/common.css?_v=${versionUpdate}`;
-// animation.rel = "stylesheet";
-// animation.href = `/baexang_front/css/animation.css?_v=${versionUpdate}`;
-// header.rel = "stylesheet";
-// header.href = `/baexang_front/css/header.css?_v=${versionUpdate}`;
-// main.rel = "stylesheet";
-// main.href = `/baexang_front/css/${pageName}.css?_v=${versionUpdate}`;
-// footer.rel = "stylesheet";
-// footer.href = `/baexang_front/css/footer.css?_v=${versionUpdate}`;
-// responsive.rel = "stylesheet";
-// responsive.href = `/baexang_front/css/responsive.css?_v=${versionUpdate}`;
-
-// for (let i = 0; i < fileNames.length; i++) {
-//   document.head.insertAdjacentElement("beforeend", fileNames[i]);
-// }
-
 /* ----- Custom JS Files ----- */
+const jsFileNames = ["customjs", "customjq"];
+
+for (let i = 0; i < jsFileNames.length; i++) {
+  const jsFile = document.createElement("script");
+  jsFile.type = "text/javascript";
+  jsFile.src = `/baexang_front/js/ui/${jsFileNames[i]}.js?_v=${versionUpdate}`;
+  document.body.insertAdjacentElement("beforeend", jsFile);
+}
+
 const include = document.createElement("script");
-const customjq = document.createElement("script");
-const customjs = document.createElement("script");
 
 include.type = "text/javascript";
 include.src = `/baexang_front/js/etc/include.js?_v=${versionUpdate}`;
-customjq.type = "text/javascript";
-customjq.src = `/baexang_front/js/ui/customjq.js?_v=${versionUpdate}`;
-customjs.type = "text/javascript";
-customjs.src = `/baexang_front/js/ui/customjs.js?_v=${versionUpdate}`;
 
 document.body.insertAdjacentElement("beforeend", include);
-document.body.insertAdjacentElement("beforeend", customjq);
-document.body.insertAdjacentElement("beforeend", customjs);
