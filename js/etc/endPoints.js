aadb4aad5d195a9d56db62767f98659cd9785720
marshallhch <marshallhch@gmail.com>
2023-05-15 12:47:07 +0900

    로그인, 로그아웃 엔드포인트 추가 0515

--- js/etc/endPoints.js
+++ js/etc/endPoints.js
@@ -2,6 +2,10 @@ var endPoints = {
   register: {
     signup:
       "https://www.dabipyeung.com/main_project_vom13/baexang_back/register/signup",
+    signin:
+      "https://www.dabipyeung.com/main_project_vom13/baexang_back/register/signin",
+    signout:
+      "https://www.dabipyeung.com/main_project_vom13/baexang_back/register/signout",
   },
   admin: {},
   productPictures: {},
